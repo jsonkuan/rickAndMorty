2b3ebecdddca28444ca82a9173b403850294d98e
jsonkuan <jsonkuan@gmail.com>
2018-06-08 17:02:32 +0200

    Initial Commit

---
+++ Rick and Morty Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rick and Morty Quiz
+//
+//  Created by Jason Kuan on 2018-06-08.
+//  Copyright © 2018 Jason Kuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Rick and Morty QuizTests/Rick_and_Morty_QuizTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rick_and_Morty_QuizTests.swift
+//  Rick and Morty QuizTests
+//
+//  Created by Jason Kuan on 2018-06-08.
+//  Copyright © 2018 Jason Kuan. All rights reserved.
+//
+
+import XCTest
+@testable import Rick_and_Morty_Quiz
+
+class Rick_and_Morty_QuizTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
